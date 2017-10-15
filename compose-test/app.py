d288c1f618643069d1fb764a840927869e92e3a5
Nick Otter <nicholas.a.otter@gmail.com>
2017-10-15 22:11:41 +0100

    Bind mount check passed - image updates automatically with any modifications - changes reflected just by refreshing browser

--- compose-test/app.py
+++ compose-test/app.py
@@ -10,7 +10,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Hello Bind Mount Test! I have been seen {} times.\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
